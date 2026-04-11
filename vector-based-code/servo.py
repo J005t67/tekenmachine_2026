6bce7f416b1f21adbca5d9199fb1fd154b0ed13e
Joost van Rooijen <joost.rzz@gmail.com>
2026-04-11 16:03:50 +0200

    Add test function to servo class

--- vector-based-code/servo.py
+++ vector-based-code/servo.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 from machine import Pin, PWM
 
@@ -22,5 +23,22 @@ class Servo:
         self.pwm.duty_u16(self.__angle_to_duty(angle_degrees))
         
     def set_angle_radians(self, angle_radians):
-        self.set_angle_degrees(math.degrees(angle_radians))       
+        self.set_angle_degrees(math.degrees(angle_radians))
         
+    def test(self):    
+       time.sleep(1.0)
+       self.set_angle_degrees(90)
+       time.sleep(2.0)
+       self.set_angle_degrees(0)
+       time.sleep(1.0)
+       self.set_angle_degrees(90)
+       time.sleep(2.0)
+       self.set_angle_degrees(180)
+       time.sleep(1.0)
+       self.set_angle_degrees(90)
+       time.sleep(2.0)
+           
+    def __test_cycle(self):
+        set_angle_degrees(90)
+        
+ 
